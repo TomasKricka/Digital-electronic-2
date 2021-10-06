8a06b8ff2ca12fc0c3aa4c69c910d9644d5234fe
student <student@PC-635.urel.feec.vutbr.cz>
2021-10-06 12:43:02 +0200

    school commit

--- Labs/03-gpio/gpio/gpio/main.c
+++ Labs/03-gpio/gpio/gpio/main.c
@@ -1,19 +1,20 @@
 /***********************************************************************
  * 
- * Alternately toggle two LEDs when a push button is pressed.
+ * Alternately toggle two LEDs when a push button is pressed. Use 
+ * functions from GPIO library.
  * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
  *
- * Copyright (c) 2018-Present Tomas Fryza
+ * Copyright (c) 2019-Present Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
- *  
+ * 
  **********************************************************************/
 
 /* Defines -----------------------------------------------------------*/
 #define LED_GREEN   PB5     // AVR pin where green LED is connected
-#define LED_WHITE   PC5
+#define LED_RED     PC5
 #define BUTTON      PD2
-#define BLINK_DELAY 800
+#define BLINK_DELAY 850
 #ifndef F_CPU
 # define F_CPU 16000000     // CPU frequency in Hz required for delay
 #endif
@@ -21,28 +22,26 @@
 /* Includes ----------------------------------------------------------*/
 #include <util/delay.h>     // Functions for busy-wait delay loops
 #include <avr/io.h>         // AVR device-specific IO definitions
-#include <avr/sfr_defs.h>
+#include "gpio.h"           // GPIO library for AVR-GCC
 
-/* Functions ---------------------------------------------------------*/
+/* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Toggle two LEDs when a push button is pressed.
+ * Purpose:  Toggle two LEDs when a push button is pressed. Functions 
+ *           from user-defined GPIO library is used.
  * Returns:  none
  **********************************************************************/
 int main(void)
 {
     // Green LED at port B
-    // Set pin as output in Data Direction Register...
-    DDRB = DDRB | (1<<LED_GREEN);
-    // ...and turn LED off in Data Register
-    PORTB = PORTB & ~(1<<LED_GREEN);
+    GPIO_config_output(&DDRB, LED_GREEN);
+    GPIO_write_high(&DDRB, LED_GREEN);
+    GPIO_write_low(&PORTB, LED_GREEN);
 
     // Configure the second LED at port C
-    
-    DDRC = DDRC | (1<<LED_WHITE);
-    
-    PORTC = PORTC & ~(1<<LED_WHITE);
-
+    GPIO_config_output(&DDRC, LED_RED);
+    GPIO_write_high(&DDRC, LED_RED);
+    GPIO_write_low(&PORTC, LED_RED);
 
     // Configure Push button at port D and enable internal pull-up resistor
     
@@ -56,40 +55,27 @@ int main(void)
     {
         // Pause several milliseconds
         
-        
+       /* GPIO_config_output(&DDRB, LED_GREEN);
         _delay_ms(BLINK_DELAY);
-        PORTB = PORTB | (1<<LED_GREEN);
+        GPIO_write_high(&DDRB, LED_GREEN);
         _delay_ms(BLINK_DELAY);
-        PORTB = PORTB &~ (1<<LED_GREEN);
-        _delay_ms(BLINK_DELAY);
-        PORTC = PORTC | (1<<LED_WHITE);
-        _delay_ms(BLINK_DELAY);
-        PORTC = PORTC &~ (1<<LED_WHITE);
-        
-       
-/*
-        
-        _delay_ms(BLINK_DELAY);
-        PORTB = PORTB | (1<<LED_GREEN);
-        PORTC = PORTC | (1<<LED_WHITE);
-        _delay_ms(BLINK_DELAY);
-        PORTB = PORTB &~ (1<<LED_GREEN);
-        PORTC = PORTC &~ (1<<LED_WHITE);
+        GPIO_write_low(&PORTB, LED_GREEN);
         */
-
         
-       
-    /*
-       
-       if (bit_is_clear(PIND ,BUTTON))
-       {
-            PORTB ^= (1<<LED_GREEN);
-            PORTC ^= (1<<LED_WHITE);
-            loop_until_bit_is_clear(PIND, BUTTON);
-       }
-       */
-       
-
+        
+        //GPIO_config_output(&DDRC, LED_RED);
+        
+        GPIO_config_output(&DDRB, LED_GREEN);
+        GPIO_config_output(&DDRC, LED_RED);
+        
+        _delay_ms(BLINK_DELAY);
+        GPIO_write_high(&DDRC, LED_RED);
+        _delay_ms(BLINK_DELAY);
+        GPIO_write_low(&DDRC, LED_RED);
+        _delay_ms(BLINK_DELAY);
+        GPIO_write_high(&DDRB, LED_GREEN);
+        _delay_ms(BLINK_DELAY);
+        GPIO_write_low(&PORTB, LED_GREEN);
 
         // WRITE YOUR CODE HERE
     }
